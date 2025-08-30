5ff4dd984b22128aa38df8ea68e755e544f3030a
MaRuF2131 <marufahmmed916@gmail.com>
2025-08-30 14:25:31 +0600

    basic setup

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Sidebar({SidebarItems=[]}) {
+  return (
+    <aside>      
+      <ul>
+        {SidebarItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
