78d320d0af7bd9bbf1a98aa831f1651ba322113c
developerTamanna <astamanna57@gmail.com>
2025-10-20 18:49:30 +0600

    coustomize

--- src/app/sazin-construction/certifications/add-certification/page.js
+++ src/app/sazin-construction/certifications/add-certification/page.js
@@ -1,26 +1,39 @@
-"use client"
-import React from 'react'
-import DynamicForm from '@/components/DynamicForm'
-import { DangerousContentCheck } from '@/utils/custom-validation/CustomValidation';
+'use client';
+import DynamicForm from '@/components/DynamicForm';
 import axiosInstance from '@/utils/axios';
+import { DangerousContentCheck } from '@/utils/custom-validation/CustomValidation';
 const fields = [
-  {name:"certificateName",placeholder:"Certificate Name", label:"Certificate Name",type:"text",rules: { required: "Image is required",...DangerousContentCheck }},
-  {name:"image", label:"Certificate Image", type: "image",  rules: { required: "Image is required" } },
+  {
+    name: 'certificateName',
+    placeholder: 'Certificate Name',
+    label: 'Certificate Name',
+    type: 'text',
+    rules: { required: 'Image is required', ...DangerousContentCheck },
+  },
+  {
+    name: 'image',
+    label: 'Certificate Image',
+    type: 'image',
+    rules: { required: 'Image is required' },
+  },
 ];
-const onSubmit = async(data) => {
-  try{
-     const response= await  axiosInstance.post('/sazin-construction/addAction/add-certificate', data)
-      return response
-  }catch(error){
-      throw error
-    }; 
-}
+const onSubmit = async (data) => {
+  try {
+    const response = await axiosInstance.post(
+      '/sazin-construction/addAction/add-certificate',
+      data
+    );
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 function page() {
   return (
     <div>
-      <DynamicForm fields={fields} onSubmit={onSubmit}/>
+      <DynamicForm fields={fields} onSubmit={onSubmit} />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
